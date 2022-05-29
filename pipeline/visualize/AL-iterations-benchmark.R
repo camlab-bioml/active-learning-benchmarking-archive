c2c5a89e6cf1ac11a13d43d0c08e23d4686c5e41
Michael-Geuenich <m.geuenich@mail.utoronto.ca>
2022-05-28 20:01:17 -0400

    make iterations bench accept more params

--- pipeline/visualize/AL-iterations-benchmark.R
+++ pipeline/visualize/AL-iterations-benchmark.R
@@ -4,35 +4,41 @@ source("pipeline/whatsthatcell-helpers.R")
 
 ### [ LOAD GROUND TRUTH ] #####
 scRNAseq <- readRDS(snakemake@input[['sce']])
-scRNAseq_gt <- tibble(cell_id = colnames(scRNAseq),
-                      annotated_cell_type = scRNAseq$CellType)
+sce <- readRDS("data/CyTOF/CyTOF-test.rds")
+expression_gt <- tibble(cell_id = colnames(sce),
+                        annotated_cell_type = sce$CellType)
 
-# AL <- list.files("output/v1/rare-subtype-benchmarking/", 
-#                  pattern = "scRNASeq-Active-Learning", full.names = TRUE)
+AL <- list.files("output/v3/rare-subtype-benchmarking/",
+                 pattern = "CyTOF-Active-Learning", full.names = TRUE)
 
 AL_files <- snakemake@input[['assignments']]
 save.image('debug')
 AL <- lapply(AL_files, read_tsv) %>% 
   bind_rows()
 
-split_params <- str_split_fixed(AL$prediction_params, "-", 4)
+split_params <- str_split_fixed(AL$prediction_params, "-", 14)
 AL$method <- paste0(split_params[,1], "-", split_params[,2])
-AL$iteration <- split_params[,4]
+AL$cell_num <- split_params[,10]
+AL$randomSelection <- split_params[,12]
+AL$corrupted <- split_params[,14]
 
-AL <- left_join(AL, scRNAseq_gt)
+AL <- left_join(AL, expression_gt)
 
 
 acc <- AL %>% 
   filter(predicted_cell_type != "unassigned") %>% 
-  group_by(method, iteration) %>% 
+  group_by(method, selection_procedure, cell_num, randomSelection, corrupted) %>% 
   acc_wrap()
 
 pdf(snakemake@output[['pdf']], width = 12, height = 4)
   acc %>% 
-    mutate(iteration = factor(as.integer(iteration))) %>% 
-    ggplot(aes(x = iteration, y = .estimate, group = iteration)) +
+    mutate(cell_num = factor(as.integer(cell_num))) %>% 
+    filter(method == "CyTOF-LDA") %>% 
+    ggplot(aes(x = cell_num, y = .estimate, color = selection_procedure, group = selection_procedure)) +
     geom_point() +
-    scale_x_discrete(breaks = acc$iteration) +
+    geom_line(aes(group = selection_procedure)) +
+    scale_x_discrete(breaks = acc$cell_num) +
+    ylim(0,1) +
     theme_bw() +
-    facet_grid(method~.metric)
+    facet_grid(randomSelection + corrupted~.metric)
 dev.off()
