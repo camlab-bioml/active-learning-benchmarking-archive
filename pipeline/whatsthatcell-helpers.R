b4f6dd1e5ae70282fd1b3ae6b65022020cbe87e9
Michael-Geuenich <michaelgeuenich@yahoo.com>
2022-09-16 15:09:57 -0400

    colours, f1, corruption & cell selection

--- pipeline/whatsthatcell-helpers.R
+++ pipeline/whatsthatcell-helpers.R
@@ -3,18 +3,15 @@
 ### [ ACTIVE LEARNING ] #####
 select_initial_cells <- function(df_expression,
                                  marker_dict,
-                                 number_cells = 1) {
-  cell_types <- names(marker_dict)
-  unannotated_cells <- df_expression %>% filter(is.na(cell_type))
-  annotated_cells <- df_expression %>% filter(!is.na(cell_type))
-  left_cell_types <-
-    setdiff(cell_types, annotated_cells$cell_type)
+                                 number_cells = 10) {
   
-  selected_cells <- lapply(left_cell_types, function(x) {
+  cell_types <- names(marker_dict)
+  
+  selected_cells <- lapply(cell_types, function(x) {
     #Negative markers
     neg_markers <- marker_dict[[x]]$negative
     if (!is.null(neg_markers)) {
-      neg_marker_expr <- unannotated_cells[, neg_markers]
+      neg_marker_expr <- df_expression[, neg_markers]
       neg_avg_expression <-
         apply(neg_marker_expr, 1, calculate_expression_average)
     } else{
@@ -23,7 +20,7 @@ select_initial_cells <- function(df_expression,
     #Positive markers
     pos_markers <- marker_dict[[x]]$positive
     if (!is.null(pos_markers)) {
-      pos_marker_expr <- unannotated_cells[, pos_markers]
+      pos_marker_expr <- df_expression[, pos_markers]
       pos_avg_expression <-
         apply(pos_marker_expr, 1, calculate_expression_average)
     } else{
@@ -31,10 +28,30 @@ select_initial_cells <- function(df_expression,
     }
     
     average_expression <- pos_avg_expression - neg_avg_expression
-    unannotated_cells$average_expression <- average_expression
-    unannotated_cells[order(unannotated_cells$average_expression, decreasing = T), ][1:number_cells, ]$X1
-  }) %>% unlist()
-  return(unique(selected_cells))
+    df <- tibble(marker = average_expression)
+    colnames(df) <- names(marker_dict[x])
+    df
+  }) %>% bind_cols()
+  
+  selected_cells <- selected_cells |> 
+    rowwise() |> 
+    mutate(max_mean_expression = max(across()),
+           max_cell_type = cell_types[which.max(c_across(everything(selected_cells)))]) |> 
+    ungroup() |> 
+    mutate(X1 = df_expression$X1) |> 
+    select(X1, max_cell_type, max_mean_expression) |> 
+    arrange(-max_mean_expression)
+    
+  cell_types_to_sample <- recycle(cell_types, number_cells)
+  
+  lapply(cell_types_to_sample, function(i){
+    cell <- filter(selected_cells, max_cell_type == i) |> 
+      slice_head(n = 1) |> 
+      pull(X1)
+    
+    selected_cells <<- filter(selected_cells, !(X1 %in% cell))
+    cell
+  }) |> unlist()
 }
 
 calculate_expression_average <- function(expressions) {
@@ -162,10 +179,21 @@ select_cells_classifier <- function(df_expression, AL_method, selection_method, 
   left_cells <- df_expression %>% 
     filter(is.na(cell_type))
   
+  training_set_size <- nrow(annotated_cells)
+  if(training_set_size < 50){
+    bootstrap_reps <- 300
+  }else if(training_set_size >=50 | training_set_size < 100){
+    bootstrap_reps <- 50
+  }else{
+    bootstrap_reps <- 25
+  }
+
+  tctrl <- trainControl(method = "boot", number = bootstrap_reps)
   ModelFit <- train(cell_type ~ ., 
                     data = select(annotated_cells, -X1),
                     method = AL_method,
-                    trace = FALSE)
+                    trace = FALSE,
+                    trControl = tctrl)
   
   predicted_scores <- predict(ModelFit, 
                               select(left_cells, -X1, -cell_type),
@@ -212,9 +240,9 @@ select_cells_classifier <- function(df_expression, AL_method, selection_method, 
 }
 
 
-cell_ranking_wrapper <- function(df, markers){
+cell_ranking_wrapper <- function(df, markers, number_cells = 10){
   # Get initial set of cells based on their marker expression ranking
-  ranked_cells <- select_initial_cells(df, markers$cell_types)
+  ranked_cells <- select_initial_cells(df, markers$cell_types, number_cells)
   
   # What index do the selected cells correspond to?
   to_assign_index <- match(ranked_cells, df$X1)
@@ -238,6 +266,29 @@ active_learning_wrapper <- function(df, AL_method, selection_method, iteration,
   list(new_cells = AL$selected_cells, criterion_table = AL$criterion_table)
 }
 
+corrupt_labels <- function(df_expression, all_cell_types, prop_corrupt){
+  num_corrupt <- round((nrow(df_expression) * as.numeric(prop_corrupt)), 0)
+  corrupt_idx <- sample(1:nrow(df_expression), num_corrupt)
+
+  for(i in 1:length(all_cell_types)){
+    tc <- all_cell_types[i]
+
+    to_corrupt <- which(df_expression$gt_cell_type == tc)
+    to_corrupt <- intersect(to_corrupt, corrupt_idx)
+    if(length(to_corrupt) > 0){
+      subset_cell_types <- all_cell_types[!(tc == all_cell_types)]
+      df_expression$corrupted[to_corrupt] <- sample(subset_cell_types, 
+                                                    length(to_corrupt), 
+                                                    replace = TRUE)
+    }
+  }
+  
+  df_expression$gt_cell_type[!is.na(df_expression$corrupted)] <- 
+    df_expression$corrupted[!is.na(df_expression$corrupted)]
+  
+  df_expression
+}
+
 
 ### [ ACCURACIES ] #####
 acc_wrap <- function(tt) {
@@ -251,6 +302,7 @@ acc_wrap <- function(tt) {
     tryCatch({bal_accuracy(tt, annotated_cell_type, predicted_cell_type)}, error=function(e) NULL),
     tryCatch({mcc(tt, annotated_cell_type, predicted_cell_type)}, error=function(e) NULL),
     tryCatch({sensitivity(tt, annotated_cell_type, predicted_cell_type)}, error=function(e) NULL),
+    tryCatch({f_meas(tt, annotated_cell_type, predicted_cell_type)}, error=function(e) NULL),
   )
 }
 
@@ -260,7 +312,8 @@ createHeatmap <- function(sce,
                           cell_type_column = "cell_type",
                           assay = "logcounts",
                           thresh = 2,
-                          title = NULL) {
+                          title = NULL,
+                          modality) {
   
   lc <- t(as.matrix(assay(sce, assay)))
   lc <- scale(lc)
@@ -274,7 +327,7 @@ createHeatmap <- function(sce,
   
   celltype_annot <- HeatmapAnnotation(`Cell type` = cell_types, 
                                       which="column",
-                                      col = list(`Cell type` = cell_type_colours()))  
+                                      col = list(`Cell type` = cell_type_colours(modality)))  
   
   if(is.null(title)){
     title <- title
@@ -295,7 +348,7 @@ createHeatmap <- function(sce,
 
 
 cell_type_colours <- function(modality) {
-  pal <- c("#8B5B42", "#AF4EA9", "#FFB60A", "#0AC694", "#0024DD", "#6CC1FF",
+  pal <- c("#8B5B42", "#AF4EA9", "#FFB60A", "#79E200", "#0024DD", "#6CC1FF",
            "#0496FF", "#1DA05B", "#E11E00", "#A78882", "#BD93D8", "#fff53d", '#FD4FBD')
   
   scRNASeq_colours <- c(
@@ -308,6 +361,19 @@ cell_type_colours <- function(modality) {
     "Megakaryocyte" = pal[9],
     "Natural killer cell" = pal[10],
     "Plasmacytoid dendritic cell" = pal[6],
+    'unassigned' = "grey60"
+  )
+  snRNASeq_colours <- c(
+    "Fibroblast" = pal[1],
+    "Tumor" = pal[2],
+    "Endothelial" = pal[9],
+    "Immune" = pal[3],
+    "Ductal" = pal[5],
+    "SmoothMuscle" = pal[10],
+    "Schwann" = pal[4],
+    "Atypical_Ductal" = pal[7],
+    "Endocrine" = pal[8],
+    "Acinar" = pal[6],
     'unassigned' = "grey60"
   )
   CyTOF_colours <- c(
@@ -324,7 +390,7 @@ cell_type_colours <- function(modality) {
   if(modality == "scRNASeq"){
     scRNASeq_colours
   }else if(modality == "snRNASeq"){
-    
+    snRNASeq_colours
   }else if(modality == "CyTOF"){
     CyTOF_colours
   }
