8381541c98beabd71a669caed467f5e0b742c978
Michael Geuenich <m.geuenich@mail.utoronto.ca>
2022-11-25 14:50:03 -0500

    small changes

--- pipeline/visualize/plot-entropies.R
+++ pipeline/visualize/plot-entropies.R
@@ -1,26 +1,11 @@
 library(tidyverse)
 library(RColorBrewer)
 library(patchwork)
+source("pipeline/whatsthatcell-helpers.R")
 
 files <- snakemake@input[['f']]
 
 ## Create plots
-create_boxplot <- function(vals, y_lab, colors){
-  box <- vals %>% 
-    mutate(no_cells_annotated = factor(no_cells_annotated)) %>% 
-    ggplot(aes(x=no_cells_annotated, y=criterion_val, 
-              group = no_cells_annotated, fill = no_cells_annotated)) + 
-    geom_boxplot() +
-    theme_bw() +
-    scale_fill_manual(values = colors) +
-    labs(y = y_lab) +
-    theme(legend.position = "none",
-          axis.text.x = element_blank(),
-          axis.title.x = element_blank(),
-          axis.ticks.x = element_blank())
-    
-  box
-}
 
 if(snakemake@wildcards[['AL_type']] == 'Active-Learning_entropy'){
   ylab = "Entropy"
@@ -34,7 +19,7 @@ plots <- lapply(files, function(x){
     length()
   colors <- colorRampPalette(brewer.pal(9, "Blues"))(col_numbers)
 
-  create_boxplot(vals, ylab, colors)
+  create_entropy_boxplot(vals, ylab, colors)
 })
 
 
