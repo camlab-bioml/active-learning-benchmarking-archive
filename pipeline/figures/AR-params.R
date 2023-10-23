6431195d7a2f73bbd56c6479ca749b6daffcbc7d
Michael Geuenich <m.geuenich@mail.utoronto.ca>
2023-10-23 13:46:13 -0400

    AR params & main heatmap

--- pipeline/figures/AR-params.R
+++ pipeline/figures/AR-params.R
@@ -11,8 +11,15 @@ scrna_acc <- read_tsv(snakemake@input$scrna_acc) |>
   mutate(cohort = "scRNASeq")
 snrna_acc <- read_tsv(snakemake@input$snrna_acc) |> 
   mutate(cohort = "snRNASeq")
+scrna_lung_acc <- read_tsv(snakemake@input$scrna_lung) |> 
+  mutate(cohort = "scRNALung")
+liverAtlas_acc <- read_tsv(snakemake@input$liver) |> 
+  mutate(cohort = "liverAtlas")
+tabulaVasc_acc <- read_tsv(snakemake@input$vasc) |> 
+  mutate(cohort = "tabulaVasc")
 
-acc <- bind_rows(cytof_acc, scrna_acc, snrna_acc)
+acc <- bind_rows(cytof_acc, scrna_acc, snrna_acc,
+                 scrna_lung_acc, liverAtlas_acc, tabulaVasc_acc)
 
 
 
@@ -43,19 +50,27 @@ plot_knn_res <- function(acc, fill, cohort){
 }
 
 
-pdf(snakemake@output$res, height = 14, width = 9)
-  (plot_knn_res(cytof_acc, "res", "CyTOF") /
-    plot_knn_res(scrna_acc, "res", "scRNASeq") /
-    plot_knn_res(snrna_acc, "res", "snRNASeq")) +
-    plot_layout(guides = "collect", heights = c(1, 2, 2))
+pdf(snakemake@output$res, 
+    height = 23, width = 15)
+  (plot_knn_res(cytof_acc, "res", "CyTOF - Bone marrow") /
+    plot_knn_res(scrna_acc, "res", "scRNASeq - Breast cancer cell lines") /
+    plot_knn_res(snrna_acc, "res", "snRNASeq - Pancreas cancer")) |
+  (plot_knn_res(scrna_lung_acc, "res", "scRNASeq - Lung cancer cell lines") /
+     plot_knn_res(liverAtlas_acc, "res", "scRNASeq - Liver") /
+     plot_knn_res(tabulaVasc_acc, "res", "scRNASeq - Vasculature")) +
+  plot_layout(guides = "collect")
 dev.off()
 
 
-pdf(snakemake@output$knn, height = 14, width = 9)
-(plot_knn_res(cytof_acc, "knn", "CyTOF") /
-    plot_knn_res(scrna_acc, "knn", "scRNASeq") /
-    plot_knn_res(snrna_acc, "knn", "snRNASeq")) +
-  plot_layout(guides = "collect", heights = c(1, 2, 2))
+pdf(snakemake@output$knn, 
+    height = 23, width = 15)
+  (plot_knn_res(cytof_acc, "knn", "CyTOF - Bone marrow") /
+      plot_knn_res(scrna_acc, "knn", "scRNASeq - Breast cancer cell lines") /
+      plot_knn_res(snrna_acc, "knn", "snRNASeq - Pancreas cancer")) |
+    (plot_knn_res(scrna_lung_acc, "knn", "scRNASeq - Lung cancer cell lines") /
+       plot_knn_res(liverAtlas_acc, "knn", "scRNASeq - Liver") /
+       plot_knn_res(tabulaVasc_acc, "knn", "scRNASeq - Vasculature")) +
+    plot_layout(guides = "collect")
 dev.off()
 
 
